91c2f05839ca0ee06676290a4635c86ef582d288
Anupam1707 <programmer.tiak@gmail.com>
2025-09-10 23:52:13 +0530

    Bugs 2.0

--- main/middleware.py
+++ main/middleware.py
@@ -5,18 +5,18 @@ class AllowIframeOnlyMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # return self.get_response(request) # for development purpose only, comment this line for production
-    
-        if request.path.startswith(settings.STATIC_URL):
-            return self.get_response(request)
+        if not settings.DEBUG:
+            if request.path.startswith(settings.STATIC_URL):
+                return self.get_response(request)
 
-        elif request.path.startswith('/admin/'):
-            return self.get_response(request)
+            elif request.path.startswith('/admin/'):
+                return self.get_response(request)
 
-        allowed_referer = 'https://nmimsindore.acm.org'
-        referer = request.META.get('HTTP_REFERER')
+            allowed_referer = 'https://nmimsindore.acm.org'
+            referer = request.META.get('HTTP_REFERER')
 
-        if referer and referer.startswith(allowed_referer):
-            return self.get_response(request)
+            if referer and referer.startswith(allowed_referer):
+                return self.get_response(request)
 
-        return HttpResponseForbidden("<h1>403 Forbidden</h1><p>Direct access is not allowed.</p>")
+            return HttpResponseForbidden("<h1>403 Forbidden</h1><p>Direct access is not allowed.</p>")
+        return self.get_response(request)
