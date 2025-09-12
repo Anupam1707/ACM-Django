0b4718cdcb78feacb96ed3c41136d715fe0b33d2
Anupam1707 <programmer.tiak@gmail.com>
2025-09-12 18:44:52 +0530

    Media Access Control

--- main/middleware.py
+++ main/middleware.py
@@ -7,8 +7,8 @@ class AllowIframeOnlyMiddleware:
     def __call__(self, request):
         if settings.DEBUG:
             return self.get_response(request)
-        
-        if request.path.startswith(settings.STATIC_URL):
+
+        if request.path.startswith(settings.STATIC_URL) or request.path.startswith(settings.MEDIA_URL):
             return self.get_response(request)
 
         elif request.path.startswith('/admin/'):
